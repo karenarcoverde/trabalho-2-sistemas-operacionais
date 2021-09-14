108c533dfe80efdb5ff50e1b6b1283b083154663
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2021-09-14 17:52:00 -0300

    Add files via upload

--- README.txt
+++ README.txt
@@ -1,1 +1,20 @@
-                                                        
+Trabalho 2 de Sistemas Operacionais (EEL770) do período de 2021.1
+Aluna: Karen dos Anjos Arcoverde
+
+Sistema operacional usado: Linux
+Distribuição: Ubuntu 20.04.1 LTS
+
+Para compilar o programa, digite no terminal:
+make
+
+Para executar o programa, digite no terminal:
+./trabalho1
+
+Para limpar os objetos e o executável, digite no terminal:
+make clean
+
+O problema implementado foi "5.3 The FIFO barbershop". 
+
+Caso queira ajustar os parâmetros, modifique nas macros contidas no código. 
+As informações são impressas na tela. Os clientes sentam nas cadeiras, o barbeiro atende o primeiro cliente a sentar, é mostrado na tela que determinado
+cliente cortou o cabelo. Quando as cadeiras já estão cheias, os clientes que chegam vão embora. 
--- trabalho2.cpp
+++ trabalho2.cpp
@@ -4,146 +4,116 @@
 #include <pthread.h>
 #include <list>
 
-#define CLIENT_NUMBER 10
+//MACROS - parâmetros que podem ser modificados pelo usuário
+#define CLIENT_NUMBER 10 //número de clientes 
+#define SIZE_FILA 5 //número de cadeiras, ou seja, número de pessoas que podem ficar na fila
 
-#define SIZE_FILA 5
 using namespace std;
 
-
-
-int finalizou = 0;
-int num_chairs = 10;
-
-
+int clientePos = 0;
+list<int> cadeiras; //lista utilizada para organizar os clientes em ordem
 
 typedef struct {
    pthread_mutex_t mutex;
    pthread_cond_t  cond;
 } cliente_struct;
 
-
-
+//para o array cliente
 pthread_mutex_t clientArray_mutex = PTHREAD_MUTEX_INITIALIZER;
 cliente_struct clientes[CLIENT_NUMBER];
-int clientePos=0;
 
+//para as cadeiras
 pthread_mutex_t cadeiras_mutex = PTHREAD_MUTEX_INITIALIZER;
-list<int> cadeiras;
 
-
+//para os clientes
 pthread_mutex_t client_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t client_ = PTHREAD_COND_INITIALIZER;
 
-
-//pthread_mutex_t barber_mutex = PTHREAD_MUTEX_INITIALIZER;
-//pthread_cond_t barber_ = PTHREAD_COND_INITIALIZER;
-
-
-
-//for exit and join
-pthread_mutex_t finishMutex = PTHREAD_MUTEX_INITIALIZER;
-pthread_cond_t finishCond = PTHREAD_COND_INITIALIZER;
-
-
-
-void thr_join_all(unsigned size) {
-    pthread_mutex_lock(&finishMutex);
-    while (((unsigned)finalizou) != size)
-        pthread_cond_wait(&finishCond, &finishMutex);
-    pthread_mutex_unlock(&finishMutex);
-    printf("Todas as threads terminaram\n");
-}
-
-
-void *barber( void *) {
-  
+void *barber( void *) { 
    cliente_struct *currentCliente;
+
+   //barbeiro está sempre cortando cabelo
    while(1) {
-      
-      /* wait for a client to become available (sem_client) */
-	  pthread_cond_wait(&client_,&client_mutex);
-      /* wait for mutex to access chair count (chair_mutex) */
+      //esperando por um cliente disponível
+      pthread_cond_wait(&client_,&client_mutex);
+      // pegando o primeiro cliente da fila - FIFO
       currentCliente = &clientes[cadeiras.front()];
       printf("Barber: atendendo cliente na posicao %i\n",cadeiras.front());
 	  
       sleep(1);
-      /* signal to client that barber is ready to cut their hair (sem_barber) */
-	  pthread_cond_signal(&currentCliente->cond);
-      /* give up lock on chair count */
+      //sinaliza para o cliente que o barbeiro está pronto para cortar o cabelo
+	   pthread_cond_signal(&currentCliente->cond);
 
-      
-      
       pthread_mutex_lock(&cadeiras_mutex);
       printf("Barber: cabelo cortado \n");
-      /* increment number of chairs available */
+      // elimina o primeiro elemento da lista de cadeiras, ou seja, vai para o próximo cliente que está na fila de cadeiras
 	   cadeiras.pop_front();
+      //como já cortou o cabelo e não precisa mais sentar na cadeira, libera o lock para outra thread entrar 
 	   pthread_mutex_unlock(&cadeiras_mutex);
-
-      /* cut hair for worktime seconds (really just call sleep()) */
-     
-	
     } 
 }
 
 void *client(void *) {
-
    int posCopy;
    pthread_mutex_lock(&clientArray_mutex);
    posCopy = clientePos;
-   clientes[posCopy].mutex=PTHREAD_MUTEX_INITIALIZER;
-   clientes[posCopy].cond=PTHREAD_COND_INITIALIZER;
+   clientes[posCopy].mutex = PTHREAD_MUTEX_INITIALIZER;
+   clientes[posCopy].cond = PTHREAD_COND_INITIALIZER;
    clientePos++;
    pthread_mutex_unlock(&clientArray_mutex);
-
+   
+   //está sempre chegnado cliente, nunca para
    while(1) {
-      /* wait for mutex to access chair count (chair_mutex) */
+     //espera pelo mutex para acessar a fila de cadeiras
 	  pthread_mutex_lock(&cadeiras_mutex);
-      /* if there are no chairs */
+     //se a fila estiver cheia e não tiver mais cadeira para sentar
 	  if(cadeiras.size() >= SIZE_FILA){
-           /* free mutex lock on chair count */
-		   printf("Client: Thread  %lu(%i) leaving with no haircut\n", pthread_self(),posCopy);
-		   pthread_mutex_unlock(&cadeiras_mutex);
+		   printf("Client: Thread  %lu(%i) saindo sem corte de cabelo\n", pthread_self(),posCopy);
+		   pthread_mutex_unlock(&cadeiras_mutex); //como nao conseguiu sentar na cadeira, libera o lock para outra thread entrar 
 	  }
-      /* else if there are chairs */
+     //se tiver cadeira para sentar
 	  else{
-           /* decrement number of chairs available */
+         
          cadeiras.push_back(posCopy);
 
-		   printf("Client: Thread  %lu(%i) Sitting to wait. Number of chairs left = %ld\n",pthread_self(),posCopy,cadeiras.size());
+		   printf("Client: Thread  %lu(%i) sentou para esperar. Numero de cadeiras ocupadas = %ld\n",pthread_self(),posCopy,cadeiras.size());
          pthread_mutex_unlock(&cadeiras_mutex);
-
-           /* signal that a customer is ready (sem_client) */
+         //sinaliza que o cliente está pronto
 		   pthread_cond_signal(&client_);
-           /* free mutex lock on chair count */
-		   
-           /* wait for barber (sem_barber) */
+         //espera pelo barbeiro
 		   pthread_cond_wait(&clientes[posCopy].cond,&clientes[posCopy].mutex);
-           /* get haircut */
-		   printf("Client: Thread  %lu(%i) getting a haircut\n",pthread_self(),posCopy);
+         //cabelo cortado
+		   printf("Client: Thread  %lu(%i) cabelo cortado\n",pthread_self(),posCopy);
 	  }
      sleep(1);
-     printf("Client: Thread %lu(%i) waiting seconds before attempting next haircut\n",pthread_self(),posCopy);
+     printf("Client: Thread %lu(%i) esperando segundos antes de tentar o proximo corte de cabelo\n",pthread_self(),posCopy);
    }
-
-
      return NULL;
 }
 
 int main() {
+   pthread_t todasthreads [CLIENT_NUMBER+1]; //vetor com as threads do barbeiro e dos clientes. É somado 1 no índice para incluir o barbeiro
+   //cria a thread para o barbeiro
+   pthread_create(&todasthreads[0], NULL, barber, NULL);
 
-   pthread_t threadsall [CLIENT_NUMBER+1];
-
-   pthread_create(&threadsall[0], NULL, barber, NULL);
-   //printf("Creating barber thread with id %lu\n",threadsall[0]);
-
-   /* 4. Create client threads.  */
+   //cria as threads para os clientes
    for (int i = 1; i < CLIENT_NUMBER+1; i++){
-	   pthread_create(&threadsall[i], NULL, client, NULL);
-	   //printf("Creating client thread with id %lu\n",threadsall[i]);
+	   pthread_create(&todasthreads[i], NULL, client, NULL);  
    }
 
-   thr_join_all (CLIENT_NUMBER);
+   //para finalizar as threads
+	for (int i = 0; i < CLIENT_NUMBER+1; i++){
+			pthread_join(todasthreads[i],NULL);
+	}
    return 0;
 }
 
-
+//Referências:
+//Adaptado a partir do codigo fornecido pelo monitor https://github.com/tonyjmartinez/Sleeping-Barber/blob/ca168a12f539f24846a87950530bb7bed2206370/mybarber.c 
+//Utilizando locks, variavéis de estado e FIFO
+//Baseado na aula variavéis de condição: https://www.youtube.com/watch?v=a-WlBjCAP9Y&list=PL6i520yqwpsT-Zjj6jf9PA8M2oPk80ypf&index=22&ab_channel=RodrigodeSouzaCouto
+//Fornecido pelo monitor: https://web.stanford.edu/~ouster/cgi-bin/cs140-spring14/lecture.php?topic=locks
+//https://pubs.opengroup.org/onlinepubs/9699919799/functions/pthread_join.html
+//https://www.cplusplus.com/reference/list/list/pop_front/
+//https://www.cplusplus.com/reference/vector/vector/front/
+//Código fornecido pelo professor: threadJoin.c
